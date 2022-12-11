fa63caa26e464053f9dcb32319b9a385341ab1c0
heap3d <heap3d@gmail.com>
2022-12-11 15:19:15 +0200

    minor flake8 edits

--- scripts/ocio_nuke_default.py
+++ scripts/ocio_nuke_default.py
@@ -13,23 +13,26 @@ import modo
 
 try:
     # change preferences settings
-    lx.eval('pref.value colormanagement.default_ocio_config nuke-default')
+    lx.eval("pref.value colormanagement.default_ocio_config nuke-default")
     lx.eval('pref.value colormanagement.8bit_default_colorspace "nuke-default:sRGB"')
     lx.eval('pref.value colormanagement.16bit_default_colorspace "nuke-default:sRGB"')
     lx.eval('pref.value colormanagement.float_default_colorspace "nuke-default:linear"')
     lx.eval('pref.value colormanagement.numeric_default_colorspace "nuke-default:sRGB"')
     lx.eval('pref.value colormanagement.view_default_colorspace "nuke-default:sRGB"')
 
-    lx.eval('pref.value colormanagement.colorMappingApplyToColorSwatches true')
+    lx.eval("pref.value colormanagement.colorMappingApplyToColorSwatches true")
 
     # change scene settings
     scene = modo.scene.current()
     scene.sceneItem.select(replace=True)
-    lx.eval('item.channel scene$ocioConfig nuke-default')
+    lx.eval("item.channel scene$ocioConfig nuke-default")
     lx.eval('item.channel scene$def8bitColorspace "nuke-default:sRGB"')
     lx.eval('item.channel scene$def16bitColorspace "nuke-default:sRGB"')
     lx.eval('item.channel scene$defFloatColorspace "nuke-default:linear"')
 
 except Exception:
-    modo.dialogs.alert('Nuke-default', 'Error while switching to Nuke-default. Change your profiles manually', 'error')
-
+    modo.dialogs.alert(
+        "Nuke-default",
+        "Error while switching to Nuke-default. Change your profiles manually",
+        "error",
+    )
